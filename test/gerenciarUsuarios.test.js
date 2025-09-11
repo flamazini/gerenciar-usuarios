f9a193f1f638d000d742867ce46f19c5cdfc236a
Flavio <flavio.mazini@gmail.com>
2025-09-11 21:19:15 +0100

    adicionei lista de usuarios e corrigi test correspondente

--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -2,12 +2,22 @@ const { retornarUsuarios, adicionarNovoUsuario } = require('../src/gerenciarUsua
 const {expect } = require('chai');
 
 describe('Testar as Funções de Gestão de Usuarios', function () {
-    it('Validar que posso adicionar um novo nome de usuárop na lista', function () {
+    it('Validar que posso adicionar um novo nome de usuário na lista', function () {
         //1. Adicionar um nome na lista de usuarios 
-        adicionarNovoUsuario('Maria');
+        adicionarNovoUsuario({
+            nome:'Maria',
+            email: 'maria@gospel.com'
+        });
+
         //2. Retornar a lista de usuarios na caixa lista de usuarios
-        const listaDeUusarios = retornarUsuarios();
+        const listaDeUsuarios = retornarUsuarios();
+
         //3. Verificar se o nome adicionado é o ultimo nome na lista
-        expect(listaDeUusarios.at(-1)).to.equal('Maria');
+        expect(listaDeUsuarios.at(-1).nome).to.equal('Maria');
+        expect(listaDeUsuarios.at(-1).email).to.equal('maria@gospel.com');
+
+        //4. Verificar o email na terceira posicao da lista
+        expect(listaDeUsuarios[2].nome).to.equal('Lucas');
+
     });
 });
